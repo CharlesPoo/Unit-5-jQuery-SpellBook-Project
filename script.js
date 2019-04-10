4a1c28705ebae00e85e3f4c95d839cb35e3adb8c
CharlesPoo <49485730+CharlesPoo@users.noreply.github.com>
2019-04-10 09:34:33 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#buttonFadeOut").click(function(){
+    $("#Lumos").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
